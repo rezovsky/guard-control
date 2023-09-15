63d078501ea91be3ad87ce702d1c884c18d05608
rezovsky <e.rezovsky@gmail.com>
2023-09-15 20:52:20 +0500

    edit api

--- DataBaseFunction.py
+++ DataBaseFunction.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from models import Events
 from sqlalchemy import func
 from flask import jsonify
@@ -23,37 +25,70 @@ class DataBaseFunction:
             self.db.session.commit()
 
     def get_all_events(self):
-        # Получаем список уникальных дат
-        unique_dates = self.db.session.query(Events.date).distinct().all()
+        # Вычисляем текущую дату и временной интервал для последних 7 дней
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=7)
 
+        # Преобразуем даты в строки в соответствии с форматом в базе данных
+        start_date_str = start_date.strftime('%Y-%m-%d')
+        end_date_str = end_date.strftime('%Y-%m-%d')
+
+        # Получаем список уникальных дат только за последние 7 дней
+        unique_dates = self.db.session.query(Events.date).distinct().filter(Events.date >= start_date_str,
+                            Events.date <= end_date_str).order_by(Events.date).all()
+        unique_dates = [date[0] for date in unique_dates]
         # Получаем список уникальных групп
-        unique_groups = self.db.session.query(Events.group).distinct().all()
+        unique_groups = self.db.session.query(Events.group).distinct().order_by(Events.group).all()
+        unique_groups = [group[0] for group in unique_groups]
 
-        # Получаем список учеников с их датами и направлением прохода
+        # Получаем список учеников с их событиями только за последние 7 дней
         students_data = self.db.session.query(
             Events.name,
             Events.group,
-            Events.date,
-            func.array_agg(func.json_build_object('action', Events.action, 'time', Events.time)).label('events')
-        ).group_by(Events.name, Events.group, Events.date).all()
+            func.json_build_object(
+                'action', Events.action,
+                'date', Events.date,
+                'time', Events.time
+            ).label('event_info')
+        ).filter(Events.date >= start_date_str, Events.date <= end_date_str).order_by(Events.name, Events.date,
+                                                                                      Events.time).all()
 
-        # Преобразуем результаты в нужный формат
-        unique_dates = [date[0] for date in unique_dates]
-        unique_groups = [group[0] for group in unique_groups]
         students_info = []
 
+        current_student = None
+        current_info = None
+
         for student in students_data:
-            student_info = {
-                'name': student.name,
-                'group': student.group,
-                'events': student.events,
-            }
-            students_info.append(student_info)
+            if student.name != current_student:
+                if current_info:
+                    students_info.append({
+                        'name': current_student,
+                        'group': current_info['group'],
+                        'events': current_info['events']
+                    })
+                current_student = student.name
+                current_info = {
+                    'group': student.group,
+                    'events': []
+                }
+
+            current_info['events'].append({
+                'action': student.event_info['action'],
+                'date': student.event_info['date'],
+                'time': student.event_info['time']
+            })
+
+        if current_info:
+            students_info.append({
+                'name': current_student,
+                'group': current_info['group'],
+                'events': current_info['events']
+            })
 
         data = {
             'unique_dates': unique_dates,
             'unique_groups': unique_groups,
-            'students_info': students_info,
+            'students_info': students_info
         }
 
         return data
