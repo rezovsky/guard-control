bf3636e21906948792fb4d8a143a374d5ff838ea
rezovsky <e.rezovsky@gmail.com>
2023-09-20 21:48:54 +0500

    edit image generator

--- Telegram.py
+++ Telegram.py
@@ -6,6 +6,7 @@ class Telegram:
     def __init__(self, db_function):
         self.db_function = db_function
         self.border_px = 1
+        self.text_padding = 6
         self.box_px = 25
         self.folder_path = 'images'
         self.image_width = 1280
@@ -21,53 +22,61 @@ class Telegram:
 
             students_data = self.db_function.get_data(group)
 
-            dates_px = len(students_data['unique_dates']['filter_date']) * (self.box_px + self.border_px)
+            dates = students_data['unique_dates']['filter_date']
+            dates_px = len(dates) * (self.box_px + (self.border_px * 2))
 
             font = ImageFont.truetype(os.path.join("font", "Arial.ttf"), size=18)
 
             name_px = 0
-            text_height_px = 0
+            name_height_px = 0
             for student in students_data['students_info'][group]:
                 text_left, text_top, text_right, text_bottom = font.getbbox(student, "")
-                text_width = text_left + text_right
-                text_height = text_top + text_bottom
-                text_height_px = max(text_height_px, text_height)
-                name_px = max(name_px, text_width) + 5
+                text_width = text_right - text_left
+                name_px = max(name_px, text_width)
+                name_height_px += self.box_px
 
-            self.image_width = name_px + dates_px
+            name_px += self.text_padding
+            self.image_width = name_px + dates_px - self.border_px
+            self.image_height = name_height_px + self.box_px
 
             # Создайте изображение и холст
 
             image = Image.new("RGB", (self.image_width, self.image_height), "white")
             draw = ImageDraw.Draw(image)
 
+            students_list = []
             # Рисуем горизонтальные линии таблицы
-            for i in range(len(students_data['students_info'][group]) + 1):
-                y = i * self.box_px
+            for index, student in enumerate(students_data['students_info'][group], start=1):
+                y = index * self.box_px + self.border_px
                 draw.line([(0, y), (self.image_width, y)], fill="black")
 
+                # Рисуем текст (имя студента) в центре ячейки
+                text_x = self.text_padding
+                text_y = y + (self.text_padding / 2)
+                draw.text((text_x, text_y), student, fill="black", font=font)
+                students_list.append(student)
+
             # Рисуем вертикальные линии таблицы
-            for i in range(len(students_data['unique_dates']['filter_date']) + 1):
-                x = i * self.box_px
+            for index, date in enumerate(students_data['unique_dates']['filter_date'], start=0):
+                x = index * (self.box_px + self.border_px) + name_px + self.text_padding + self.border_px
                 draw.line([(x, 0), (x, self.image_height)], fill="black")
 
-            # Заполняем ячейки таблицы данными
-            for row, student_name in enumerate(student):
-                for col, date in enumerate(students_data['unique_dates']['filter_date']):
-                    # Определите цвет для ячейки в соответствии с вашей логикой
-                    color = "green"  # Замените на свой цвет
+                status_list = self.db_function.get_status_by_group_of_date(date)[group]
+                for row_number, student in enumerate(students_list, start=1):
+                    if student in status_list:
+                        status = status_list[student]
+                        if status == 'выход':
+                            color = 'red'
+                        else:
+                            color = 'green'
+                    else:
+                        color = 'gray'
 
-                    # Рисуем квадрат
-                    x0 = col * self.box_px
-                    y0 = row * self.box_px
+                    x0 = x + self.border_px
+                    y0 = row_number * self.box_px + (self.border_px * 2)
                     x1 = x0 + self.box_px
-                    y1 = y0 + self.box_px
+                    y1 = y0 + self.box_px - (self.border_px * 2)
                     draw.rectangle([x0, y0, x1, y1], fill=color)
-
-                    # Рисуем текст (имя студента) в центре ячейки
-                    text_x = (x0 + x1) / 2
-                    text_y = (y0 + y1) / 2
-                    draw.text((text_x, text_y), student_name, fill="black", font=font)
 
             # Сохраняем изображение
             image.save(output_path)
