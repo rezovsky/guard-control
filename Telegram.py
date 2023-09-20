b5aef395eea9d35a195e40f78712887453dcfdd0
rezovsky <e.rezovsky@gmail.com>
2023-09-20 23:21:54 +0500

    edit image generator

--- Telegram.py
+++ Telegram.py
@@ -12,6 +12,7 @@ class Telegram:
         self.image_width = 1280
         self.image_height = 1024
         self.background_color = (255, 255, 255)
+        self.image_border = 20
 
     def get_images(self):
         if not os.path.exists(self.folder_path):
@@ -36,30 +37,39 @@ class Telegram:
                 name_height_px += self.box_px
 
             name_px += self.text_padding
-            self.image_width = name_px + dates_px - self.border_px
-            self.image_height = name_height_px + self.box_px
+            self.image_width = name_px + dates_px - self.border_px + (self.image_border * 2)
+            self.image_height = name_height_px + self.box_px + (self.image_border * 2)
 
             # Создайте изображение и холст
 
             image = Image.new("RGB", (self.image_width, self.image_height), "white")
             draw = ImageDraw.Draw(image)
 
+            draw.text((self.image_border, self.image_border), group, fill="black", font=font)
+
             students_list = []
             # Рисуем горизонтальные линии таблицы
             for index, student in enumerate(students_data['students_info'][group], start=1):
-                y = index * self.box_px + self.border_px
-                draw.line([(0, y), (self.image_width, y)], fill="black")
+                y = index * self.box_px + self.border_px + self.image_border
+                draw.line([(self.image_border, y), (self.image_width - self.image_border - self.border_px, y)], fill="black")
 
                 # Рисуем текст (имя студента) в центре ячейки
-                text_x = self.text_padding
+                text_x = self.text_padding + self.image_border
                 text_y = y + (self.text_padding / 2)
                 draw.text((text_x, text_y), student, fill="black", font=font)
                 students_list.append(student)
 
             # Рисуем вертикальные линии таблицы
             for index, date in enumerate(students_data['unique_dates']['filter_date'], start=0):
-                x = index * (self.box_px + self.border_px) + name_px + self.text_padding + self.border_px
-                draw.line([(x, 0), (x, self.image_height)], fill="black")
+                x = index * (self.box_px + self.border_px) + name_px + self.text_padding + self.border_px + self.image_border
+                draw.line([(x, self.image_border), (x, self.image_height - self.image_border)], fill="black")
+
+                day_number = date.split('-')[2]
+                day_number_px_array = font.getbbox(day_number, "")
+                day_number_px = day_number_px_array[2] - day_number_px_array[0]
+                day_number_x = (self.box_px - day_number_px) / 2
+
+                draw.text(((x + day_number_x), self.image_border), day_number, fill="black", font=font)
 
                 status_list = self.db_function.get_status_by_group_of_date(date)[group]
                 for row_number, student in enumerate(students_list, start=1):
@@ -73,7 +83,7 @@ class Telegram:
                         color = 'gray'
 
                     x0 = x + self.border_px
-                    y0 = row_number * self.box_px + (self.border_px * 2)
+                    y0 = row_number * self.box_px + (self.border_px * 2) + self.image_border
                     x1 = x0 + self.box_px
                     y1 = y0 + self.box_px - (self.border_px * 2)
                     draw.rectangle([x0, y0, x1, y1], fill=color)
